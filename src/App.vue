66ab3ebcae210b8a3a8e42c5dde55e4a05afb8d3
FelipeDNL <felipednlled@gmail.com>
2025-04-01 17:53:41 -0300

    ideia de design

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,114 @@
 </script>
 
 <template>
-  <div id="app">
+  <div class="content">
 
-    <div class="content">
-      <h1>Welcome to Your Vue.js App</h1>
-    
-    <div class="scrolling-letters">
-      <p>This is a simple Vue.js application.</p>
+    <div class="main-page">
+      <h1>フィリップ</h1>
     </div>
 
-  </div>
+    <div class="letters">
+      <div class="scrolling-letters-1">
+        <p>日本語の話し方がわかりません</p>
+      </div>
+
+      <div class="scrolling-letters-2">
+        <p>Meu app vue.js</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.content {
+  display: flex;
+  height: 98vh;
+  overflow: hidden;
+  white-space: nowrap;
+}
 
-  .content {
-    display: flex;
+.main-page {
+  flex: 1;
+}
+
+.scrolling-letters-1 {
+  writing-mode: vertical-rl;
+  font-size: 60px;
+  color: brown;
+  padding-right: 40px;
+
+  /* animation properties */
+  -moz-transform: translateY(-100%);
+  -webkit-transform: translateY(-100%);
+  transform: translateY(-100%);
+
+  -moz-animation: my-animation-2 3s linear infinite;
+  -webkit-animation: my-animation-2 3s linear infinite;
+  animation: my-animation-2 3s linear infinite;
+}
+
+.scrolling-letters-2 {
+  writing-mode: sideways-rl;
+  font-size: 120px;
+  color: brown;
+
+  /* animation properties */
+  -moz-transform: translateY(100%);
+  -webkit-transform: translateY(100%);
+  transform: translateY(100%);
+
+  -moz-animation: my-animation-1 15s linear infinite;
+  -webkit-animation: my-animation-1 15s linear infinite;
+  animation: my-animation-1 15s linear infinite;
+}
+
+/* for Chrome */
+@-webkit-keyframes my-animation-1 {
+  from {
+    -webkit-transform: translateY(100%);
   }
 
-  .scrolling-letters {
-    flex: 1;
-    writing-mode: sideways-rl;
-    white-space: nowrap;
-    font-size: 120px;
-    color: rgb(199, 12, 12);
+  to {
+    -webkit-transform: translateY(-100%);
+  }
+}
+
+@keyframes my-animation-1 {
+  from {
+    -moz-transform: translateY(100%);
+    -webkit-transform: translateY(100%);
+    transform: translateY(100%);
   }
 
+  to {
+    -moz-transform: translateY(-100%);
+    -webkit-transform: translateY(-100%);
+    transform: translateY(-100%);
+  }
+}
+
+/* for Chrome */
+@-webkit-keyframes my-animation-2 {
+  from {
+    -webkit-transform: translateY(-100%);
+  }
+
+  to {
+    -webkit-transform: translateY(100%);
+  }
+}
+
+@keyframes my-animation-2 {
+  from {
+    -moz-transform: translateY(-100%);
+    -webkit-transform: translateY(-100%);
+    transform: translateY(-100%);
+  }
+
+  to {
+    -moz-transform: translateY(100%);
+    -webkit-transform: translateY(100%);
+    transform: translateY(100%);
+  }
+}
 </style>
