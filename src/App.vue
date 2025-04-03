3ab4bc453d4b7c12b6fffe6b6f1a711283646cd1
FelipeDNL <felipednlled@gmail.com>
2025-04-02 22:15:33 -0300

    design pronto

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
 
 <template>
   <div class="content">
-
     <div class="main-page">
-      <h1>フィリップ</h1>
+      <h1>日本語の話し方がわかりません</h1>
     </div>
 
     <div class="letters">
       <div class="scrolling-letters-1">
-        <p>日本語の話し方がわかりません</p>
+        <p>フィリップ</p>
       </div>
 
       <div class="scrolling-letters-2">
-        <p>Meu app vue.js</p>
+        <p class="p-big">Life's getting quite exciting</p>
       </div>
+
     </div>
   </div>
 </template>
@@ -24,92 +24,69 @@
 .content {
   display: flex;
   height: 98vh;
-  overflow: hidden;
-  white-space: nowrap;
+  overflow: hidden; /* Impede a barra de rolagem */
+  
 }
 
 .main-page {
-  flex: 1;
+  flex:1;
+  display: flex;
+}
+
+.letters {
+  white-space: nowrap; /* Impede a quebra de linha */
+}
+
+.p-big {
+  margin-right: 60px
 }
 
 .scrolling-letters-1 {
   writing-mode: vertical-rl;
   font-size: 60px;
   color: brown;
-  padding-right: 40px;
 
   /* animation properties */
-  -moz-transform: translateY(-100%);
-  -webkit-transform: translateY(-100%);
-  transform: translateY(-100%);
+  transform: translateY(100%);
 
-  -moz-animation: my-animation-2 3s linear infinite;
-  -webkit-animation: my-animation-2 3s linear infinite;
-  animation: my-animation-2 3s linear infinite;
+  animation: animacao-cima-baixo 4s linear infinite; /* Aplica a animação `animacao-baixo-cima` no padrão, com duração de 10s, linear e infinita. */
 }
 
 .scrolling-letters-2 {
-  writing-mode: sideways-rl;
-  font-size: 120px;
-  color: brown;
+  writing-mode: sideways-rl; /* Define o texto para ser escrito na direção horizontal, mas rotacionado para a direita. */
+  font-size: 120px; /* Define o tamanho da fonte como 120px. */
+  color: brown; /* Define a cor do texto como marrom. */
+  /* Propriedades de transformação inicial */
+  transform: translateY(-100%); /* Move o elemento 100% para baixo no eixo Y (padrão). */
 
-  /* animation properties */
-  -moz-transform: translateY(100%);
-  -webkit-transform: translateY(100%);
-  transform: translateY(100%);
-
-  -moz-animation: my-animation-1 15s linear infinite;
-  -webkit-animation: my-animation-1 15s linear infinite;
-  animation: my-animation-1 15s linear infinite;
+  /* Propriedades de animação */
+   animation: animacao-baixo-cima 10s linear infinite; /* Aplica a animação `animacao-cima-baixo` no padrão, com duração de 10s, linear e infinita. */
 }
 
-/* for Chrome */
-@-webkit-keyframes my-animation-1 {
+@keyframes animacao-baixo-cima {
   from {
-    -webkit-transform: translateY(100%);
+    transform: translateY(35%); /* Início da animação no padrão: o elemento está 100% abaixo do eixo Y. */
   }
 
   to {
-    -webkit-transform: translateY(-100%);
+    transform: translateY(-115%); /* Fim da animação no padrão: o elemento está 100% acima do eixo Y. */
   }
 }
 
-@keyframes my-animation-1 {
+@keyframes animacao-cima-baixo {
   from {
-    -moz-transform: translateY(100%);
-    -webkit-transform: translateY(100%);
-    transform: translateY(100%);
+    transform: translateY(-120%);
   }
 
   to {
-    -moz-transform: translateY(-100%);
-    -webkit-transform: translateY(-100%);
-    transform: translateY(-100%);
+    transform: translateY(320%);
   }
 }
 
-/* for Chrome */
-@-webkit-keyframes my-animation-2 {
-  from {
-    -webkit-transform: translateY(-100%);
+@media (max-width: 768px) {
+  .p-big {
+    margin-right: 0px;
   }
-
-  to {
-    -webkit-transform: translateY(100%);
-  }
-}
-
-@keyframes my-animation-2 {
-  from {
-    -moz-transform: translateY(-100%);
-    -webkit-transform: translateY(-100%);
-    transform: translateY(-100%);
-  }
-
-  to {
-    -moz-transform: translateY(100%);
-    -webkit-transform: translateY(100%);
-    transform: translateY(100%);
-  }
+  
 }
 </style>
